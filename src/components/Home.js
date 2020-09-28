264e5975aa1660ef88614b29439070f7c8f08195
Patrick White <patrick.cf.white@gmail.com>
2020-09-28 17:58:42 +0100

    tweaked frontpage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
+    <>
     <section className="slideContainer hero is-fullheight">
       <div data-aos="fade-up" className="hero-body">
         <div className="container">
@@ -9,7 +10,12 @@ const Home = () => {
           <h2 className="subtitle">Web Developer</h2>
         </div>
       </div>
+    <div className="hero-foot container">
+      <img className='bounce' src={require('../assets/white-down-arrow-png-2.png')} alt="down arrow"/>
+    </div>
     </section>
+
+  </>
   )
 }
 
