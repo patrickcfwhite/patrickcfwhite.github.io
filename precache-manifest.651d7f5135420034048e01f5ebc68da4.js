819e0a149cf2caffd6a5eb383ecbc6e469e0c967
Patrick White <patrick.cf.white@gmail.com>
2020-09-28 13:13:13 +0100

    Updates

---
+++ precache-manifest.651d7f5135420034048e01f5ebc68da4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8254300e05abd2bb6b24fc4fd34083",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a341a1dbbc5b44fc11c",
+    "url": "/static/css/2.7f7d5e0a.chunk.css"
+  },
+  {
+    "revision": "970a249700ddf09db7c9",
+    "url": "/static/css/main.a48d528e.chunk.css"
+  },
+  {
+    "revision": "4a341a1dbbc5b44fc11c",
+    "url": "/static/js/2.af4b43dc.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/static/js/2.af4b43dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970a249700ddf09db7c9",
+    "url": "/static/js/main.01047c55.chunk.js"
+  },
+  {
+    "revision": "367ecdb168df314c692f",
+    "url": "/static/js/runtime-main.ea8a729d.js"
+  },
+  {
+    "revision": "0f964a72f8fbdf9c8d4e0122b8effb40",
+    "url": "/static/media/devicon.0f964a72.woff"
+  },
+  {
+    "revision": "29f80b00a7b4641183f96f08be374697",
+    "url": "/static/media/devicon.29f80b00.ttf"
+  },
+  {
+    "revision": "681fa39a79c22f0035a0720e2b2bda3b",
+    "url": "/static/media/devicon.681fa39a.eot"
+  },
+  {
+    "revision": "74e6e6c3dcfca56767adabe83a510f2e",
+    "url": "/static/media/devicon.74e6e6c3.svg"
+  }
+]);
